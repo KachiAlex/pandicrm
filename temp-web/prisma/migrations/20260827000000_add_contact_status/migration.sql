aae0a5f8088951652f2c9998f3643aa8c48dc832
KachiAlex <kachiAlex@users.noreply.github.com>
2026-09-02 06:47:41 +0100

    chore: remove temp-web artifact

--- temp-web/prisma/migrations/20260827000000_add_contact_status/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "ContactStatus" AS ENUM ('new', 'qualified', 'opportunity', 'customer', 'lost');
-
--- AlterTable
-ALTER TABLE "contacts" ADD COLUMN "status" "ContactStatus" NOT NULL DEFAULT 'new';
-
--- CreateIndex
-CREATE INDEX "contacts_status_idx" ON "contacts"("status");
--- temp-web/prisma/migrations/20260827000001_add_user_plan/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "UserPlan" AS ENUM ('free', 'standard', 'premium');
-
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "plan" "UserPlan" NOT NULL DEFAULT 'free';
